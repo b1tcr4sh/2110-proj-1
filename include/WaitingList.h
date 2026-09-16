84ee3e528223f34e7a84c51ed683831779c07b30
Madison Lusk <MadisonLusk@my.unt.edu>
2026-09-16 12:31:34 -0500

    include/WaitingList.h

---
+++ include/WaitingList.h
@@ -0,0 +1,29 @@
+#ifndef WaitingList_h
+#define WaitingList_h
+
+#include <iostream>
+#include <queue>
+
+class WaitingList {
+    public:
+    queue<Request> waitingList;
+
+
+
+}
+
+
+class Request {
+    public:
+
+
+
+
+
+}
+
+
+
+
+
+#endif
