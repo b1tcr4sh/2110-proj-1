9e4e2504fc04dd6e9d646bcb1ae6e8bb4bd243e5
Madison Lusk <MadisonLusk@my.unt.edu>
2026-09-16 17:04:31 -0500

    src/Resource.cpp

--- src/Resource.cpp
+++ src/Resource.cpp
@@ -6,3 +6,23 @@ Resource::Resource(string id, string name, ResourceType type, bool available) {
     this->type = type;
     this->available = available;
 }
+
+void Resource::addToWaitingList(string studentId){
+    WaitingList.push(studentId);
+}
+
+void Resource::removeFromWaitingList(){
+    WaitingList.pop();
+}
+
+void Resource::displayWaitingList() {
+    queue<string> copy = WaitingList;
+
+    cout << "Waiting List: " << endl;
+
+    while (!copy.empty()) {
+        cout << copy.front() << endl;
+        copy.pop();
+    }
+        cout << endl;
+}
